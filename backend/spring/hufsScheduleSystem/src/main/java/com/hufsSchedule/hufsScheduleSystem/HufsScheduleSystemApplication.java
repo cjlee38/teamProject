82ec907afd36dd7bc3435a7cb4f09c70d39787dd
최기환 <directorgihwan@gmail.com>
2020-04-05 14:10:04 +0900

    spring set

---
+++ backend/spring/hufsScheduleSystem/src/main/java/com/hufsSchedule/hufsScheduleSystem/HufsScheduleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hufsSchedule.hufsScheduleSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HufsScheduleSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HufsScheduleSystemApplication.class, args);
+	}
+
+}
---
+++ backend/spring/hufsScheduleSystem/src/test/java/com/hufsSchedule/hufsScheduleSystem/HufsScheduleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hufsSchedule.hufsScheduleSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HufsScheduleSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
