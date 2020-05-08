d6f4f2fc0e44c8ce1056875a55f08f8320bcc91e
kjh03160 <kjh03160@likelion.org>
2020-05-08 20:24:16 +0900

    test

--- frontend/src/page/MainPage/MainPage.js
+++ frontend/src/page/MainPage/MainPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Button from './components/Button';
 import Title from './components/Title';
 import Logo from '../../image/logo.png';
@@ -9,52 +9,48 @@ import Check from '../Homepage/Check';
 import LoginPage from '../LoginPage/LoginPage';
 
 
-function MainPage (match) {
+function MainPage(match) {
     console.log(match)
     return (
-        <Router> 
-         
-                 
- 
-
-                <Switch>
+        <Router>
+            <Switch>
 
                 <Route path="/" exact={true} component={Main} />
-                <Route path="/Main" component={Main}/>
+                <Route path="/Main" component={Main} />
 
-                <Route path="/Main/:id/:password" component={Main}/>
+                <Route path="/Main/:id/:password" component={Main} />
                 <Route path="/Login" component={LoginPage} />
 
-                    <Route path="/Check" component={Check} />
-                    <Route path="/Recommend" component={Recommend} />
-                </Switch>
+                <Route path="/Check" component={Check} />
+                <Route path="/Recommend" component={Recommend} />
+            </Switch>
 
-                
-   
+
+
         </Router>
 
     )
-} 
+}
 
 
-const Main = ({match}) => {
+const Main = ({ match }) => {
     console.log(match.params.id)
     return (
-        
+
         <div className="Main">
             {/* <img src={Logo} className="logo" alt="logo"/> */}
             <div className="body">
-                <Title/>
+                <Title />
                 <Link to="/Check">
-                    <Button name={"졸업요건 확인하기"} value={"check"}/>
+                    <Button name={"졸업요건 확인하기"} value={"check"} />
                 </Link>
                 <Link to="/Recommend">
-                    <Button name={"시간표 추천받기"} value={"check"}/>
+                    <Button name={"시간표 추천받기"} value={"check"} />
                 </Link>
                 <Link to="/Login">
-                    <Button name={"로그인"} value={"check"}/>
+                    <Button name={"로그인"} value={"check"} />
                 </Link>
-               
+
             </div>
         </div>
     )
