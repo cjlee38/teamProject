9cfc9974ed235d9084ea604fba10fdebe27a989a
kjh03160 <kjh03160@likelion.org>
2020-05-10 22:47:17 +0900

    spinner

--- frontend/src/page/Homepage/components/Crawler.js
+++ frontend/src/page/Homepage/components/Crawler.js
@@ -4,88 +4,87 @@ import Tooltip from 'react-bootstrap/Tooltip';
 import Popover from 'react-bootstrap/Popover';
 import PopoverContent from 'react-bootstrap/PopoverContent';
 import PopoverTitle from 'react-bootstrap/PopoverTitle';
-import Button from '@material-ui/core/Button';  
+import Button from '@material-ui/core/Button';
 import React, { Component } from 'react';
 import api from '../crawl'
 import TextField from '@material-ui/core/TextField';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import Spinner from './Spinner'
 
 
 
-
-class popover extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            std: '',
-            password: '',}
-        
+class crawler extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      std: '',
+      password: '',
+      loading: false,
     }
 
-    handlingChange = (event) => {
-        this.setState({ [event.target.name]: event.target.value })
-        console.log(event.target.name, "촘  ")
-    }
+  }
 
-    handlingSubmit = async (event) => {
-        // event.preventDefault()
-        console.log("크롤링 시작")
-        let result = await api.crawlUser({ std_num: this.state.std, password: this.state.password })
-        console.log(result.data)
-        this.setState({ std: "", password: "" })
-    }
+  handlingChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value, loading: false })
+    console.log(event.target.name, "change  ")
+  } 
 
-    render () {
-        console.log("크롤링 시작")
+  handlingSubmit = (event) => {
+    console.log("크롤링 시작")
+    event.preventDefault()
 
-        return (
+    this.setState({ loading: true }, (event) => {
+      console.log(this.state.std, this.state.password)
 
-        <Popover id="popover-basic">
-        <Popover.Title as="h3">Update</Popover.Title>
-        <Popover.Content>            
-            <div className="Crwaler">
-      <form onSubmit={this.handlingSubmit}>
+      api.crawlUser({ std_num: this.state.std, password: this.state.password })
+        .then(result => this.setState({
+          loading: false,
+          data: [...result.data],
+        }))
+        .catch(function (error) {alert(error)});
+    });
 
+    // let result = await 
+    console.log(this.data)
+    this.setState({ std: "", password: "" })
+  }
 
-        <TextField
-          className="input" margin="normal"
-          id="standard-textarea"
-          label="학번"
-          name="std" variant="outlined"
-          placeholder="학번" value={this.state.std} onChange={this.handlingChange}
-        />
-        <TextField
-          className="input" margin="normal"
-          id="standard-textarea"
-          label="종합정보시스템 비밀번호"
-          name="password" variant="outlined"
-          placeholder="종정시 비밀번호" value={this.state.password} type="password" onChange={this.handlingChange}
-        />
-        <Button startIcon={<CloudUploadIcon />} variant="outlined" size="large" color="primary" type="submit" onSubmit={this.handlingSubmit} className="crawlingbtn">
-          업데이트      </Button>
-      </form>
-    </div>
-      </Popover.Content>
-    </Popover>
-
-    )}
-
-
-}
-
-function Example (){
+  render() {
+    const { loading } = async () => {await this.state.loading}
+    console.log(loading)
 
     return (
-<OverlayTrigger trigger="click" placement="right" children={popover}>
-    <Button variant="success">Click me to see</Button>
-  </OverlayTrigger>
 
-    );
+      <div className="Crwaler">
+        <form onSubmit={this.handlingSubmit}>
+
+
+          <TextField
+            className="input" margin="normal"
+            id="standard-textarea"
+            label="학번"
+            name="std" variant="outlined"
+            placeholder="학번" value={this.state.std} onChange={this.handlingChange}
+          />
+          <TextField
+            className="input" margin="normal"
+            id="standard-textarea"
+            label="종합정보시스템 비밀번호"
+            name="password" variant="outlined"
+            placeholder="종정시 비밀번호" value={this.state.password} type="password" onChange={this.handlingChange}
+          />
+
+
+          {loading ? <Spinner /> : <Button startIcon={<CloudUploadIcon />} variant="outlined" size="large" color="primary" type="submit" onSubmit={this.handlingSubmit} className="crawlingbtn">
+              업데이트      </Button>}
+
+        </form>
+      </div>
+    )
+  }
 }
 
-  
-
-  
 
 
-export default Example
+
+export default crawler;
