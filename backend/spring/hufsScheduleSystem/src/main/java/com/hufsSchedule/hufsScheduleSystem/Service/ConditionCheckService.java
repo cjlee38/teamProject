7e6e0f650844af0687380aad46a0e493e3322c89
최기환 <directorgihwan@gmail.com>
2020-05-11 21:23:55 +0900

    userIns

--- backend/spring/hufsScheduleSystem/src/main/java/com/hufsSchedule/hufsScheduleSystem/Service/ConditionCheckService.java
+++ backend/spring/hufsScheduleSystem/src/main/java/com/hufsSchedule/hufsScheduleSystem/Service/ConditionCheckService.java
@@ -1,5 +1,6 @@
 package com.hufsSchedule.hufsScheduleSystem.Service;
 
+import com.hufsSchedule.hufsScheduleSystem.Advice.Exception.UserNotFoundException;
 import com.hufsSchedule.hufsScheduleSystem.Dto.ConditionDto;
 import com.hufsSchedule.hufsScheduleSystem.Entity.Credit;
 import com.hufsSchedule.hufsScheduleSystem.Entity.Instruction;
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -21,9 +24,10 @@ public class ConditionCheckService {
     private final UserRepository userRepository;
 
     public ConditionDto.ResultOfCondition checkCondition(Long userId){
-        Optional<User> user = userRepository.findById(userId);
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
         Credit credit = creditRepositorySupport.findByUser(userId);
         List<Instruction> courses = courseRepositorySupport.findInstructionByUser(userId);
+        List<String> instructionName = courses.stream().map(Instruction::getSubject).collect(Collectors.toList());
 
         ConditionDto.ResultOfCondition res = ConditionDto.ResultOfCondition.builder()
                 .firstMajor(credit.getFirstMajor())
@@ -36,7 +40,7 @@ public class ConditionCheckService {
                 .optional(credit.getOptional())
                 .totalCredit(credit.getTotalCredit())
                 .averageScore(credit.getAverageScore())
-                .instructions(courses)
+                .instructions(instructionName)
                 .build();
 
         return res;
