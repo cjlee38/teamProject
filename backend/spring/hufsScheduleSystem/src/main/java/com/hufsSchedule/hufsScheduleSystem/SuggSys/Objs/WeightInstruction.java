240b1398d51dff7858595e9abec66060e79b4684
cjlee <gptpem38@gmail.com>
2020-06-07 15:22:51 +0900

    converting table to result object

--- backend/spring/hufsScheduleSystem/src/main/java/com/hufsSchedule/hufsScheduleSystem/SuggSys/Objs/WeightInstruction.java
+++ backend/spring/hufsScheduleSystem/src/main/java/com/hufsSchedule/hufsScheduleSystem/SuggSys/Objs/WeightInstruction.java
@@ -6,11 +6,22 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class WeightInstruction{
+public class WeightInstruction implements Comparable<WeightInstruction>{
     private Float weight;
     private Instruction instruction;
     public WeightInstruction(Float weight, Instruction instruction) {
         this.weight = weight;
         this.instruction = instruction;
+    }
+
+
+    @Override
+    public int compareTo(WeightInstruction o) {
+        if (this.weight < o.getWeight()) {
+            return -1;
+        } else if (this.weight > o.getWeight()) {
+            return 1;
+        }
+        return 0;
     }
 }
