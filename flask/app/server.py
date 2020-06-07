2ee206aa586d993976ef12dbafcef1379348d0f1
kjh03160 <kjh03160@likelion.org>
2020-06-07 17:02:15 +0900

    flask remove redis

--- flask/app/server.py
+++ flask/app/server.py
@@ -105,29 +105,21 @@ class ReigstInst(Resource):
         return {'status': 'success'}
 
     def get(self):
-        print(session)
-        if session.get('session_key'):
-            key = session['session_key']
-            
         
+
+        db_class = database.Database()
+
+        sql_inst_search = """SELECT instruction_id, dept, area, year, subject, url, required, professor, time,
+        credit, class_time, number_of_people, note FROM instruction WHERE rq_year={rq_year} and rq_semester={rq_semester};"""
+        sql_inst_search = sql_inst_search.format(rq_year=20, rq_semester=1)
+        row = db_class.execute_all(sql_inst_search)
+        sql_lib_area = """SELECT distinct area FROM instruction where rq_year={rq_year} and rq_semester={rq_semester} and not (area like \"%전공\" or area like \"인문학공통\" or area like \"%이중%\" or area like \"교직\");
+""".format(rq_year=20, rq_semester=1)
+        area =  db_class.execute_all(sql_lib_area)
+        if len(row):
+            return {'data' : row, 'lib' : area}
         else:
-            session['session_key'] = key
-            
-            print(session)
-
-            db_class = database.Database()
-
-            sql_inst_search = """SELECT instruction_id, dept, area, year, subject, url, required, professor, time,
-            credit, class_time, number_of_people, note FROM instruction WHERE rq_year={rq_year} and rq_semester={rq_semester};"""
-            sql_inst_search = sql_inst_search.format(rq_year=20, rq_semester=1)
-            row = db_class.execute_all(sql_inst_search)
-            sql_lib_area = """SELECT distinct area FROM instruction where rq_year={rq_year} and rq_semester={rq_semester} and not (area like \"%전공\" or area like \"인문학공통\" or area like \"%이중%\" or area like \"교직\");
-    """.format(rq_year=20, rq_semester=1)
-            area =  db_class.execute_all(sql_lib_area)
-            if len(row):
-                return {'data' : row, 'lib' : area}
-            else:
-                return {'error' : '강의가 없습니다.'}
+            return {'error' : '강의가 없습니다.'}
 
 
 
