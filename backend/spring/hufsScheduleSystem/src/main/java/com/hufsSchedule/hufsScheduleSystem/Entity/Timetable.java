83e1d0d3f843f11a9be70c00da33cf35fc625e13
최기환 <directorhwan@naver.com>
2020-06-21 13:10:13 +0900

    check

--- backend/spring/hufsScheduleSystem/src/main/java/com/hufsSchedule/hufsScheduleSystem/Entity/Timetable.java
+++ backend/spring/hufsScheduleSystem/src/main/java/com/hufsSchedule/hufsScheduleSystem/Entity/Timetable.java
@@ -19,7 +19,7 @@ public class Timetable {
     private Long tableId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_table", insertable = true, updatable = true)
+    @JoinColumn(name = "user_table", insertable = false, updatable = false)
     private User user;
 
     public void setUser(User user) {
@@ -31,7 +31,7 @@ public class Timetable {
     }
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "timetable_inst_num", insertable = true, updatable = true)
+    @JoinColumn(name = "timetable_inst_num", insertable = false, updatable = false)
     private Instruction instruction;
 
     public void setInstruction(Instruction instruction) {
